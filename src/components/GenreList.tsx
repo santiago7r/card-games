f94912b7fb819413d3a8acbfac15fe69c1a02c20
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-01-23 11:25:18 -0500

    Hightlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,9 +5,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
     onSelectGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     const { data, isLoading, error } = useGenres();
 
     if (error) return null;
@@ -24,7 +25,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
                         borderRadius={8}
                         src={getCroppedImageUrl(genre.image_background)}
                     />
-                    <Button onClick={() => onSelectGenre(genre)} variant='link'>
+                    <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} variant='link'>
                         {genre.name}
                     </Button>
                 </HStack> 
