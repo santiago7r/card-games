e5895d92939a85f1535ab6a197de43034f84cbe4
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-01-23 17:07:03 -0500

    Platforms menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from './components/GameGrid.js';
 import GenreList from './components/GenreList.js';
 import { useState } from 'react';
 import { Genre } from './hooks/useGenres.js';
+import PlatformSelector from './components/PlatformSelector.js';
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main" >
+        <PlatformSelector />
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
