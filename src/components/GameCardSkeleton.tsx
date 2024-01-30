3a6c355e8d8f78e6061d70e2fe64f8f851611284
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-01-30 18:12:39 -0500

    Solve build errors

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,8 +1,6 @@
 import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
-import useGames from "../hooks/useGame";
 
 const GameCardSkeleton = () => {
-    const { games, error } = useGames();
 
     return (
         <Card>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,6 +2,10 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
+interface Props {
+    onSearch: (searchText: string) => void;
+}
+
 const SearchInput = ({ onSearch }: Props) => {
     const ref = useRef<HTMLInputElement>(null);
 
