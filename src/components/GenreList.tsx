7c5352476eb5604c908d7567ea0e7a87feb8cbfa
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-01-30 08:51:41 -0500

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,5 @@
 
-import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
+import { Button, HStack, Heading, Image, List, ListItem, Spinner } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
@@ -16,23 +16,32 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     if (isLoading) return <Spinner />
 
     return (
-        <List>
-         {
-            data.map(genre => <ListItem key={genre.id} paddingY='5px'>
-                <HStack>
-                    <Image
-                        boxSize='32px'
-                        borderRadius={8}
-                        src={getCroppedImageUrl(genre.image_background)}
-                    />
-                    <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} variant='link'>
-                        {genre.name}
-                    </Button>
-                </HStack> 
-            </ListItem>)
-         }
+        <>
+            <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+            <List>
+            {
+                data.map(genre => <ListItem key={genre.id} paddingY='5px'>
+                    <HStack>
+                        <Image
+                            boxSize='32px'
+                            borderRadius={8}
+                            objectFit='cover'
+                            src={getCroppedImageUrl(genre.image_background)}
+                        />
+                        <Button
+                            fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} 
+                            variant='link'
+                            whiteSpace='normal'
+                            textAlign='left'
+                        >
+                            {genre.name}
+                        </Button>
+                    </HStack> 
+                </ListItem>)
+            }
 
-        </List>
+            </List>
+        </>
     )
 }
 
